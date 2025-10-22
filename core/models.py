7fa86ac5b015e4aaa2c4eeea16b04a177ee325c9
Yabtsega Aschalew <yabtsegaaschalew1@gmail.com>
2025-10-22 17:14:36 +0300

    updating-user-table-requirements

--- core/models.py
+++ core/models.py
@@ -1,12 +1,13 @@
-# Step 3 Models (3NF - FINAL)
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 
 class User(AbstractUser):
-    """Extended User model - adds currency"""
     date_joined = models.DateTimeField(default=timezone.now)
     currency = models.CharField(max_length=3, default='USD')
+    first_name = models.CharField( max_length=150, blank=False)
+    last_name = models.CharField( max_length=150, blank=False)
+    email = models.EmailField(blank=False)
     groups = models.ManyToManyField(
         'auth.Group',
         related_name='core_user_set',
@@ -31,6 +32,7 @@ class Category(models.Model):
     )
     class Meta:
         ordering = ['name']
+        db_table = "tblCategory"
     
     def __str__(self):
         return self.name
@@ -49,6 +51,7 @@ class Transaction(models.Model):
             models.Index(fields=['user', 'date']), 
             models.Index(fields=['category']),
         ]
+        db_table = "tblTransaction"
     
     def __str__(self):
         return f"{self.category.name}: ${self.amount}"
@@ -71,3 +74,4 @@ class Budget(models.Model):
     class Meta:
         unique_together = ['user', 'category', 'month']
         indexes = [models.Index(fields=['user', 'month'])]
+        db_table = "tblBudget"
