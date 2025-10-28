bf87c58b7e1484a11294dfae325b360893ad410f
Yabtsega Aschalew <yabtsegaaschalew1@gmail.com>
2025-10-29 01:27:34 +0300

    login-functionality

--- core/urls.py
+++ core/urls.py
@@ -5,16 +5,20 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
 
     path("sign-up/", views.sign_up),
-    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("reset-password/", auth_views.PasswordResetView.as_view(), name="password_reset"),
     path("reset-password/done", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path("reset-password/<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("reset-password-complete/done", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
-    path("change-password/", auth_views.PasswordChangeView.as_view(), name="password_change"),
+    # To be advanced later
+    path("change-password/", views.change_password, name="password_change"),
     path("change-password/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    # Till here
     path("activate/", views.activate_account, name="activate_account"),
-    path("activate/<str:uidb64>/<str:token>", views.activate_account_confirm, name="activate-account-confirm")
+    path("activate/<str:uidb64>/<str:token>", views.activate_account_confirm, name="activate-account-confirm"),
+    path("create-budget/", views.create_budget),
+    path("login/", views.user_login),
+    path("create-transaction/", views.create_transaction),
 
     
 ]
--- core/views.py
+++ core/views.py
@@ -1,34 +1,69 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
-from core.serializers import UserSerializer
+from core.serializers import UserSerializer, BudgetSerializer, LoginSerializer, TransactionSerializer, ChangePasswordSerializer
 from core.models import User
 from rest_framework.response import Response
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_str, force_bytes
-
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 token_generator = PasswordResetTokenGenerator()
 
 @api_view(["POST", "GET"])
 def sign_up(request):
     if request.method == "POST":
-        username = request.data.get("username")
-        email = request.data.get("email")
+        serializer = UserSerializer(data = request.data)
+        username = serializer.validated_data.get("username")
+        email = serializer.validated_data.get("email")
 
         if User.objects.filter(username=username).exists():
             return Response({"message": "Username taken"}, status=status.HTTP_403_FORBIDDEN)
         if User.objects.filter(email=email).exists():
             return Response({"message": "Email already taken"}, status=status.HTTP_403_FORBIDDEN)
         
-        serializer = UserSerializer(data = request.data)
+        
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
     elif request.method == "GET":
         return Response(User.objects.all().values())
-    
+
+@api_view(["POST"])
+def user_login(request):
+    if request.method == "POST":
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        username = serializer.validated_data.get("username")
+        password = serializer.validated_data.get("password")
+
+        if not username or not password:
+            return Response({
+                "message": "Username and Password are required"
+            }, status=status.HTTP_401_UNAUTHORIZED)
+        elif username and password:
+            user = authenticate(username=username, password=password)
+            activation_status = User.objects.get(username=username)
+            if activation_status.is_active and user:
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                }, status=status.HTTP_200_OK)
+            
+            elif not activation_status.is_active:
+                return Response({
+                    "message": "Account is not active"
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({
+                    "message": "Incorrect credentials"
+                }, status=status.HTTP_401_UNAUTHORIZED)
 
 @api_view(["POST"])
 def activate_account(request):
@@ -62,7 +97,6 @@ def activate_account_confirm(request, uidb64, token):
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
-        print(user)
 
         if token_generator.check_token(user, token):
             user.is_active = True
@@ -72,6 +106,40 @@ def activate_account_confirm(request, uidb64, token):
             return Response({"message": "Invalid or expired token"}, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         return Response({"error": "Invalid activation link"}, status=status.HTTP_400_BAD_REQUEST)
+    
 
-      
+@permission_classes([IsAuthenticated])
+@api_view(["POST"])
+def change_password(request):
+    if request.method == "POST":
+        serializer = ChangePasswordSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        
+        user = request.user
+        user.set_password(serializer.validated_data['new_password'])
+        user.save(update_fields=['password'])
+
+        return Response(
+            {"message": "Password changed successfully"},
+            status=status.HTTP_200_OK
+        )
+    
+    
+@permission_classes([IsAuthenticated])
+@api_view(["POST"])
+def create_budget(request):
+    if request.method == "POST":
+        serializer = BudgetSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+@permission_classes([IsAuthenticated])
+@api_view(["POST"])
+def create_transaction(request):
+    if request.method == "POST":
+        serializer = TransactionSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
