c49344d20581ead05e98ca5a1019d6d414f2c88d
Yabtsega Aschalew <yabtsegaaschalew1@gmail.com>
2025-11-11 00:54:08 +0300

    budget calculation logic

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,8 @@
-import uuid, requests
+import requests
 from rest_framework import status
 from rest_framework.response import Response
 from finance_tracker.settings import CHAPA_PRIVATE_KEY
+from core.models import Budget, Transaction
 
 url = "https://api.chapa.co/v1"
 private_key = CHAPA_PRIVATE_KEY
@@ -66,7 +67,7 @@ def payment_gateway(amount, email, first_name, last_name, request, phone_number,
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-# Verify payment
+
 
 
 def verify_payment(tx_ref):
@@ -79,4 +80,51 @@ def verify_payment(tx_ref):
     data = response.text
     print(data)
     return Response(data)
+
+def calculate_user_payment(user_id, payment_done=False, tx_ref=None):
+
+    budgets = Budget.objects.filter(user_id=user_id).select_related(
+        "category", "transaction"
+    )
+
+    total_income = sum(b.amount for b in budgets if b.category.type == "income")
+    total_expenses = sum(b.amount for b in budgets if b.category.type == "expense")
+
+    amount_to_pay = total_expenses
+    remaining_budget = total_income - total_expenses
+
+    if not payment_done:
+        return {
+            "amount_to_pay": amount_to_pay,
+            "status": "pending_payment",
+            "remaining_budget": remaining_budget
+        }
+
+    if payment_done:
+        with transaction.atomic():
+            
+            tx = Transaction.objects.filter(tx_ref=tx_ref, user_id=user_id).first()
+
+            if not tx:
+                return {"error": "Transaction not found."}
+
+            
+            for b in budgets.filter(category__type="expense"):
+                b.transaction = tx
+                b.save()
+
+            
+            for b in budgets.filter(category__type="income"):
+                b.amount = remaining_budget if remaining_budget > 0 else 0
+                b.save()
+
+        return {
+            "amount_to_pay": amount_to_pay,
+            "status": "payment_done",
+            "remaining_budget": remaining_budget,
+            "transaction_ref": tx_ref
+        }
+
+    
+
     
