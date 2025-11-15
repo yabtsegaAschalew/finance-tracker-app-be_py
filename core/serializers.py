ed0446ec159b71086a586e208f8b5e0f43c68d69
Yabtsega Aschalew <yabtsegaaschalew1@gmail.com>
2025-11-15 03:17:34 +0300

    bank-transfer-logic and saving phone number

--- core/serializers.py
+++ core/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["username", "first_name", "last_name", "email", "currency", "password"]
+        fields = ["username", "first_name", "last_name", "email", "phone_number", "currency", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
--- core/utils.py
+++ core/utils.py
@@ -124,7 +124,113 @@ def calculate_user_payment(user_id, payment_done=False, tx_ref=None):
             "remaining_budget": remaining_budget,
             "transaction_ref": tx_ref
         }
-
     
+    
+def bank_transfer(account_number, amount, tx_ref, bank_code):
+    api_url = f"{url}/transfers"
+
+    payload = {
+        "account_number": account_number,
+        "amount": amount,
+        "currency": "ETB",
+        "reference": tx_ref,
+        "bank_code": bank_code
+    }
+
+    headers = {
+        'Authorization': f'Bearer {private_key}',
+        'Content-Type': 'application/json'
+    }
+    try:
+        response = requests.post(api_url, json=payload, headers=headers)
+
+        if not response.text.strip():
+            return Response(
+                {"error": "Empty response from chapa"},
+                status=status.HTTP_502_BAD_GATEWAY,
+            )
+        data = response.json()
+
+        if response.status_code == 200 and data.get("status") == "success":
+            message = data["message"]
+            transaction_ref = data["data"]
+            return Response(
+                {
+                    "message": message,
+                    "tx_ref": transaction_ref,
+                },
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {
+                    "error": data,
+                    "status": data.get("status")
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    except Exception as e:
+        return Response(
+            {
+                "error": str(e),
+                "status": status.HTTP_500_INTERNAL_SERVER_ERROR
+            }
+        )
+    
+def get_bank():
+
+    api_url = f"{url}/banks"
+    payload = ''
+    headers = {
+        'Authorization': f'Bearer {private_key}'
+    }
+
+    response = requests.get(api_url, headers=headers, data=payload)
+    data = response.json()
+
+    return Response(
+        {
+            "data": data
+        }
+    )
+
+def initiate_payment(tx_ref, phone_number=None):   
+
+    api_url = f"{url}/charges?type=telebirr"
+    dataList = []
+    boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
+    dataList.append('--' + boundary)
+    dataList.append('Content-Disposition: form-data; name="amount"')
+    dataList.append('')
+    dataList.append('10')
+
+    dataList.append('--' + boundary)
+    dataList.append('Content-Disposition: form-data; name="currency"')
+    dataList.append('')
+    dataList.append('ETB')
+
+    dataList.append('--' + boundary)
+    dataList.append('Content-Disposition: form-data; name="tx_ref"')
+    dataList.append('')
+    dataList.append(tx_ref)
+
+    dataList.append('--' + boundary)
+    dataList.append('Content-Disposition: form-data; name="mobile"')
+    dataList.append('')
+    dataList.append('0900123456' if phone_number == None else phone_number)
+
+    dataList.append('--' + boundary + '--')
+    dataList.append('')
+    body = '\r\n'.join(dataList)
+    payload = body.encode('utf-8')
+    headers = {
+        'Authorization': 'Bearer CHASECK_TEST-mgWrd2rhogka8FINIdgfl7wM2Yo1mpwL',
+        'Content-type': 'multipart/form-data; boundary={}'.format(boundary)
+    }
+
+    response = requests.post(api_url, data=payload, headers=headers)
+    data = response.json()
+
+    return data
 
     
