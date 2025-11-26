d493e17d47af5d901f790e2598af05224e1ad9b5
yabtsegaAschalew <yabtsegaaschalew1@gmail.com>
2025-11-26 11:04:06 +0300

    testing-deployment

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["https://finance-tracker-app-be-py.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 CORS_ALLOWED_ORIGINS = [
