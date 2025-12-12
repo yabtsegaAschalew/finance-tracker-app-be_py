716bdad72b3654ca5674718fd77d23062adae73e
Yabtsega Aschalew <yabtsegaaschalew1@gmail.com>
2025-12-13 00:03:57 +0300

    setting up docker and server ready deployment script
    Add Docker compose command to start services

--- script.sh
+++ script.sh
@@ -2,6 +2,12 @@ python3.12 -m venv venv
 
 source venv/bin/activate
 
+docker compose up -d
+
 pip install -r requirements.txt
 
 python manage.py migrate
+
+gunicorn finance_tracker.wsgi
+
+celery -A finance_tracker worker -l info
