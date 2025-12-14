31dab3ec096263c5e21de33eac207e87e037752b
Yabtsega Aschalew <yabtsegaaschalew1@gmail.com>
2025-12-14 23:30:45 +0300

    Update CORS allowed origins for frontend access

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = ["*"]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080", 
-    "https://finance-tracker-2v2v.onrender.com"
+    "https://personal-finance-tracker-app-fe-ts.vercel.app"
 ]
 
 
